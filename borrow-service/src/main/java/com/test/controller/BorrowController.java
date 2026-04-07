502793565c6fe6b02c3861d9304c130f74fdaaad
Murasame-Chyan <3565066138@qq.com>
2026-04-07 09:54:36 +0800

    学习ing

--- borrow-service/src/main/java/com/test/controller/BorrowController.java
+++ borrow-service/src/main/java/com/test/controller/BorrowController.java
@@ -4,13 +4,18 @@ import com.test.entity.Borrow;
 import com.test.entity.BorrowDetail;
 import com.test.service.BorrowService;
 import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+// 本注解表示配置中取的东西（如链接内容）支持热更新 - 例为下列的@Value与接口/test
+@RefreshScope
 public class BorrowController {
 	@Resource
 	private BorrowService borrowService;
@@ -33,4 +38,13 @@ public class BorrowController {
 //		System.out.println("[备选方案] 服务发生降级！");
 //		return new BorrowDetail(null, Collections.emptyList());
 //	}
+
+	@Value("${test.str}")
+	private String teststr;
+
+	@RequestMapping("/borrow/test")
+	@ResponseBody
+	public String test(){
+		return teststr;
+	}
 }
